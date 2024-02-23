effdc697e45b5cc004f5d5e05c902690b03886e7
bhnord <b.nordemann@aol.com>
2024-02-22 20:58:11 -0500

    messaging looking better

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //server setup
-const express = require('express')
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-
+const Y_SPEED = 10;
+const X_SPEED = 10;
 const CANVAS_HEIGHT = 750;
 const CANVAS_WIDTH = 1400;
 const SHIP_PLATFORM = 718;
-const PLAYER_VERTICAL_INCREMENT = 20;
 const PLAYER_VERTICAL_MOVEMENT_UPDATE_INTERVAL = 1000;
 const PLAYER_SCORE_INCREMENT = 5;
 const P2_WORLD_TIME_STEP = 1 / 16;
@@ -19,82 +19,80 @@ const GAME_TICKER_MS = 100;
 const players = new Map();
 let currId = 0;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html")
-  console.log("dd")
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  console.log("dd");
+});
 
-app.get('/js/script.js', (req, res) => {
-  res.sendFile(__dirname + "/js/script.js")
-})
-app.get('/css/style.css', (req, res) => {
-  res.sendFile(__dirname + "/css/style.css")
-})
+app.get("/js/script.js", (req, res) => {
+  res.sendFile(__dirname + "/js/script.js");
+});
+app.get("/css/style.css", (req, res) => {
+  res.sendFile(__dirname + "/css/style.css");
+});
 
-app.get('/avatarPengu', (req, res) => {
-  res.sendFile(__dirname + "/assets/chinstrap.png")
-})
+app.get("/avatarPengu", (req, res) => {
+  res.sendFile(__dirname + "/assets/chinstrap.png");
+});
 
-app.get('/player', (req, res) => {
-  res.sendFile(__dirname + "/assets/pixilart-sprite.png")
-})
+app.get("/player", (req, res) => {
+  res.sendFile(__dirname + "/assets/pixilart-sprite.png");
+});
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   let newPlayer = {
-    id: currId += 1,
+    id: currId,
+    displayName: "Player " + (currId += 1),
     positionX: 0,
     positionY: 0,
-    anim: "front"
-  }
-  players.set(socket, newPlayer)
-  io.emit('move', Array.from(players.values()))
-  console.log("connected")
+    anim: "front",
+  };
+  players.set(socket, newPlayer);
+  io.emit("move", Array.from(players.values()));
+  console.log("connected");
 
-  socket.on('disconnect', () => {
-    console.log("disconnected")
-    players.delete(socket)
-  })
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+    players.delete(socket);
+  });
 
   //TODO: FIX CONCURRENCY
-  socket.emit('playerId', currId)
+  socket.emit("playerId", currId);
 
+  socket.on("msg", (msg) => {
+    io.emit("msg", players.get(socket).displayName + ": " + msg);
+    console.log("chat: " + msg);
+  });
 
-  socket.on('msg', (msg) => {
-    io.emit('msg', msg)
-    console.log("chat: " + msg)
-  })
-
-  const Y_SPEED = 10;
-  const X_SPEED = 10;
-  socket.on('move', (msg) => {
+  socket.on("move", (msg) => {
     let move = JSON.parse(msg);
-    let player = players.get(socket)
+    let player = players.get(socket);
 
     if (move.moveX === -1) {
-      player.positionX -= X_SPEED
+      player.positionX -= X_SPEED;
       player.anim = "left";
     } else if (move.moveX === 1) {
-      player.positionX += X_SPEED
+      player.positionX += X_SPEED;
       player.anim = "right";
     }
 
     if (move.moveY === -1) {
-      player.positionY -= Y_SPEED
+      player.positionY -= Y_SPEED;
       player.anim = "up";
     } else if (move.moveY === 1) {
-      player.positionY += Y_SPEED
+      player.positionY += Y_SPEED;
       player.anim = "down";
     }
 
     if (move.moveX === 0 && move.moveY === 0) {
-      player.anim = "front"
+      player.anim = "front";
     }
 
-    io.emit('move', Array.from(players.values()))
+    io.emit("move", Array.from(players.values()));
     //    console.log("player " + player.id + " " + JSON.stringify(player));
-  })
-})
+  });
+});
 
 server.listen(3000, () => {
-  console.log('listening on http://localhost:3000')
-})
+  console.log("listening on http://localhost:3000");
+});
--- js/script.js
+++ js/script.js
@@ -1,46 +1,38 @@
 let socket = io();
 let myId = -1;
 let spritesMap = new Map();
-let players = []
+let players = [];
 let isStopped = true;
-const chat = document.getElementById('chat')
-const messages = document.getElementById('messages')
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const gameContainer = document.getElementById('game-container');
+const chat = document.getElementById("chat");
+const messages = document.getElementById("messages");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const chatBox = document.getElementById("chatroom");
 
 //TODO: changeme
 const BASE_SERVER_URL = "http://localhost:3000";
 
-
-
-socket.on('move', function(p) {
+socket.on("move", function (p) {
   //set players to where they belong
   players = Array.from(p);
   //console.log(players)
-  console.log(p)
-})
+  console.log(p);
+});
 
+socket.on("msg", function (msg) {
+  let message = document.createElement("li");
+  message.textContent = msg;
+  messages.appendChild(message);
+  messages.scrollTo(0, messages.scrollHeight);
+});
 
-socket.on('msg', function(msg) {
-  let message = document.createElement("li")
-  message.textContent = msg
-  messages.appendChild(message)
-  chat.scrollTo(0, chat.scrollHeight);
-})
-
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
-    socket.emit('msg', input.value)
-    input.value = '';
+    socket.emit("msg", input.value);
+    input.value = "";
   }
-})
-
-
-
-
-
+});
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -49,100 +41,96 @@ class GameScene extends Phaser.Scene {
 
   //load assets
   preload() {
-    this.load.setBaseURL(BASE_SERVER_URL)
-    this.load.spritesheet(
-      "player",
-      "player",
-      {
-        frameWidth: 82,
-        frameHeight: 107
-      }
-    )
+    this.load.setBaseURL(BASE_SERVER_URL);
+    this.load.spritesheet("player", "player", {
+      frameWidth: 82,
+      frameHeight: 107,
+    });
   }
 
   //init vars, define animations + sounds, display assets
   create() {
-    this.cursors = this.input.keyboard.addKeys(
-      {
-        up: Phaser.Input.Keyboard.KeyCodes.W,
-        down: Phaser.Input.Keyboard.KeyCodes.S,
-        left: Phaser.Input.Keyboard.KeyCodes.A,
-        right: Phaser.Input.Keyboard.KeyCodes.D
-      });
+    this.cursors = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
 
     this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+      key: "down",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 2 }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
     this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
+      key: "right",
+      frames: this.anims.generateFrameNumbers("player", { start: 3, end: 5 }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
     this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers('player', { start: 6, end: 9 }),
+      key: "up",
+      frames: this.anims.generateFrameNumbers("player", { start: 6, end: 9 }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
     this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('player', { start: 10, end: 12 }),
+      key: "left",
+      frames: this.anims.generateFrameNumbers("player", { start: 10, end: 12 }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
     this.anims.create({
-      key: 'front',
-      frames: [{ key: 'player', frame: 0 }],
+      key: "front",
+      frames: [{ key: "player", frame: 0 }],
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
   }
 
   //update attr of game objects per game logic
   update() {
-    this.publishInput()
+    this.publishInput();
     for (let player of players) {
       if (!spritesMap.has(player.id)) {
-        let sprite = this.physics.add.sprite(player.positionX, player.positionY, 'player');
-        spritesMap.set(player.id, sprite)
+        let sprite = this.physics.add.sprite(
+          player.positionX,
+          player.positionY,
+          "player",
+        );
+        spritesMap.set(player.id, sprite);
       } else {
         //set sprite to correct location
-        let curr = spritesMap.get(player.id)
-        curr.setPosition(player.positionX, player.positionY)
+        let curr = spritesMap.get(player.id);
+        curr.setPosition(player.positionX, player.positionY);
 
         //set animations
         switch (player.anim) {
           case "left":
-            curr.anims.play('left', true)
+            curr.anims.play("left", true);
             break;
           case "right":
-            curr.anims.play('right', true)
+            curr.anims.play("right", true);
             break;
           case "up":
-            curr.anims.play('up', true)
+            curr.anims.play("up", true);
             break;
           case "down":
-            curr.anims.play('down', true)
+            curr.anims.play("down", true);
             break;
           default:
-            curr.anims.play('front', true)
+            curr.anims.play("front", true);
         }
       }
     }
   }
 
-
-
-
   publishInput() {
     let move = {
       moveX: 0,
-      moveY: 0
-    }
+      moveY: 0,
+    };
 
     if (this.cursors.up.isDown) {
       move.moveY = -1;
@@ -161,39 +149,37 @@ class GameScene extends Phaser.Scene {
     }
 
     if (move.moveX != 0 || move.moveY != 0) {
-      socket.emit('move', JSON.stringify(move));
+      socket.emit("move", JSON.stringify(move));
     } else if (!isStopped) {
-      socket.emit('move', JSON.stringify(move));
+      socket.emit("move", JSON.stringify(move));
       isStopped = true;
     }
   }
 }
+
 const config = {
   type: Phaser.AUTO,
   scale: {
     mode: Phaser.Scale.FIT,
-    parent: "game-container",
-    width: 1400,
-    height: 750
+    parent: "game-container-inside",
   },
   backgroundColor: "#FFFFF",
   scene: [GameScene],
   physics: {
-    default: "arcade"
+    default: "arcade",
   },
 };
+
 const game = new Phaser.Game(config);
 
-document.body.addEventListener('click', function(event) {
-  if (gameContainer.contains(event.target)) {
+document.body.addEventListener("click", function (event) {
+  if (chatBox.contains(event.target)) {
     //    game.input.enabled = true;
-    game.input.keyboard.enabled = true;
-    console.log("in")
-  } else {
     game.input.keyboard.enabled = false;
+    console.log("in");
+  } else {
+    game.input.keyboard.enabled = true;
     //   game.input.enabled = false;
-    console.log("out")
+    console.log("out");
   }
-})
-
-
+});
